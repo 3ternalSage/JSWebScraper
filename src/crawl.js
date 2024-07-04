f6c21204ae676c96df5cad60311283818ae89e6f
Aaditya Timalsina <aadityatimal@gmail.com>
2024-07-03 23:44:41 -0700

    crawl over domain

--- src/crawl.js
+++ src/crawl.js
@@ -11,9 +11,69 @@ function normalizeURL(url) {
 
 function getURLsFromHTML(html, url) {
     const JSDOMobj = new JSDOM(html)
-    let anchors = Array.from(JSDOMobj.window.document.querySelectorAll('a')).map(anchor => normalizeURL(new URL(anchor.href, url).toString()))
+    let anchors = Array.from(JSDOMobj.window.document.querySelectorAll('a')).map(anchor => new URL(anchor.href, url).toString())
     return anchors
 }
 
+function notSameDomain(one, other) {
+    // console.log(`'${one}' | '${other}'`)
+    const baseURL = new URL(one)
+    const otherURL = new URL(other)
+    // console.log(`'${baseURL.host}' | '${otherURL.host}' = ${baseURL.host === otherURL.host}`)
+    return baseURL.host !== otherURL.host
+}
 
-export { normalizeURL, getURLsFromHTML };
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+    if (notSameDomain(baseURL, currentURL)) {
+        return pages
+    }
+    
+    const normURL = normalizeURL(currentURL)
+
+    if (!pages[normURL]) {
+        pages[normURL] = 1
+    } else {
+        pages[normURL]++
+        return pages
+    }
+
+    console.log(`Crawling ${currentURL}`)
+
+    let pageHTML
+    try {
+        pageHTML = await fetchPage(currentURL)
+    } catch (err) {
+        console.log(`${err.message}`)
+        return pages
+    }
+
+    if (!pageHTML) {
+        return pages
+    }
+
+    const pageURLS = getURLsFromHTML(pageHTML, currentURL)
+    for (let i = 0; i < pageURLS.length; i++) {
+        pages = await crawlPage(baseURL, pageURLS[i], pages)
+    }
+    return pages
+}
+
+async function fetchPage(url) {
+    let resp
+    try {
+        resp = await fetch(url)
+    } catch(err) {
+        throw new Error(`Network error: ${err.message}`)
+    }
+
+    if (resp.status >= 400) {
+        throw new Error(`Error: Status code {${resp.status}}: {${resp.statusText}}`)
+    }
+
+    if (!resp.headers.get('content-type') || !resp.headers.get('content-type').includes('text/html')) {
+        throw new Error(`Error: Returned content type not 'text/html' but '${resp.headers.get('content-type')}'`)
+    }
+    return await resp.text()
+}
+
+export { normalizeURL, getURLsFromHTML, crawlPage };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+import { argv } from 'process'
+import { crawlPage, normalizeURL } from './crawl.js'
 
+async function main() {
+    if (argv.length !== 3) {
+        console.log("Error: Exactly one argument is required\nUsage: 'npm run start {site to begin crawl}'")
+    } else {
+        console.log(`Crawler is starting with baseURL: {${argv[2]}}`)
+    }
+    const baseURL = argv[2]
+    const pagesCrawled = await crawlPage(baseURL)
+    console.log(pagesCrawled)
+}
 
-console.log('hello world')
+main()
